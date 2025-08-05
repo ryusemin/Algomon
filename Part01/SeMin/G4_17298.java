97266dfc451ed03a09bf5734d2c3a1d14e7fc335
ryusemin <tpals3799@naver.com>
2025-08-05 23:33:58 +0900

    feat: [BOJ]_17298_오큰수 (풀이참고)

---
+++ Part01/SeMin/G4_17298.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] ss = br.readLine().split(" ");
+        int[] arr = new int[ss.length];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i =0; i< ss.length; i++){
+            arr[i] = Integer.parseInt(ss[i]);
+        }
+
+
+        for(int i =0; i< arr.length; i++){
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                    arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.println(sb);
+
+    }
+}
