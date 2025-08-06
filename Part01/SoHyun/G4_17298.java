ebca39d8397cffe5f797a13dc31de19ba56b3781
SoHyun Park <pshsh0516@naver.com>
2025-08-06 21:59:29 +0900

    feat: [BOJ]_17298_오큰수

--- Part01/SoHyun/G4_17298.java
+++ Part01/SoHyun/G4_17298.java
@@ -1,9 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class G4_17298 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i] + " ");
+        }
+
+        System.out.println(sb);
     }
 }
