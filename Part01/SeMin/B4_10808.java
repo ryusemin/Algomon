d693b56e6d63a0e2a86d46e1a28d264a3d775870
ryusemin <tpals3799@naver.com>
2025-08-10 03:19:20 +0900

    [BOJ]_10808_알파벳 개수

---
+++ Part01/SeMin/B4_10808.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        for(int i = 0; i < 26; i++){
+            char w;
+            int count = 0;
+            for(int j= 0; j < word.length(); j++){
+                w = word.charAt(j);
+                if( w - 'a' == i) count++;
+            }
+
+            System.out.print(count + " ");
+        }
+    }
+}
