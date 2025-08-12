a346d41a6984e7e001c4f5b49399b05bdda3a122
SoHyun Park <pshsh0516@naver.com>
2025-08-12 19:40:51 +0900

    [BOJ]_2743_단어 길이 재기

---
+++ Part01/SoHyun/B5_2743.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class B5_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s.length());
+    }
+}
+
