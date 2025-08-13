3f9a6211e187159a34ec28d11a7a195394d16f03
JH Kim <rlawogus65@gmail.com>
2025-08-13 17:15:14 +0900

    [BOJ]_10430_나머지

---
+++ Part02/JaeHyeon/B5_10430.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_10430 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        sb.append((A + B) % C).append("\n");
+        sb.append(((A % C) + (B % C)) % C).append("\n");
+        sb.append((A * B) % C).append("\n");
+        sb.append(((A % C) * (B % C)) % C);
+
+        System.out.println(sb.toString());
+    }
+}
