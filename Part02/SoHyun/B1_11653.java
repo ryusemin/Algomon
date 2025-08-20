f0ca2959c3687289c201b9b4af84273b596b2b3f
SoHyun Park <pshsh0516@naver.com>
2025-08-21 01:58:32 +0900

    [BOJ]_11653_소인수분해

---
+++ Part02/SoHyun/B1_11653.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class B1_11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                sb.append(i).append("\n");
+                n /= i;
+            }
+        }
+
+        if (n != 1) {
+            sb.append(n);
+        }
+        System.out.println(sb);
+    }
+}
+
