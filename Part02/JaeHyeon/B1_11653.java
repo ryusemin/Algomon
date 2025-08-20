b3386296040a949551f5bfca523f82b651a2f33c
JH Kim <rlawogus65@gmail.com>
2025-08-20 22:10:29 +0900

    [BOJ]_11653_소인수분해

---
+++ Part02/JaeHyeon/B1_11653.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B1_11653 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n != 1) {
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    sb.append(i).append("\n");
+                    n /= i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
