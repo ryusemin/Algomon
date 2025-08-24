fe23818f71d3deddea78d28d9167968a6892582c
ryusemin <tpals3799@naver.com>
2025-08-24 23:56:58 +0900

    [BOJ]11726_2×n 타일링

---
+++ Part03/SeMin/S3_11726.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class S3_11726 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n <= 2) {
+            System.out.println(n);
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
