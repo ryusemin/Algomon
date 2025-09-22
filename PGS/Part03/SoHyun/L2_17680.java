bc71ae54137e825b00a914a9142c07ee3daa8129
SoHyun Park <pshsh0516@naver.com>
2025-09-22 23:40:10 +0900

    [PGS]_17680_[1차] 캐시

---
+++ PGS/Part03/SoHyun/L2_17680.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        Queue<String> q = new LinkedList<>();
+        int time = 0;
+
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toLowerCase();
+
+            if (!q.contains(city)) {
+                time += 5;
+            } else {
+                q.remove(city);
+                time += 1;
+            }
+            q.add(city);
+
+            if (q.size() > cacheSize) {
+                q.poll();
+            }
+        }
+        return time;
+    }
+}
