492e0db784fd7789fd6f368795c46d316ad56bb5
ryusemin <tpals3799@naver.com>
2025-09-28 23:56:25 +0900

    [PGS]_42890_ 후보키

---
+++ PGS/Part03/SeMin/L2_42890.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class Solution{
+        List<Integer> answer = new ArrayList<>();
+    
+        public int solution(String[][] relation) {
+            int n = relation.length; 
+            int m = relation[0].length;
+
+            for (int i = 1; i < 1 << m; i++) {
+                Set<String> set = new HashSet<>();
+                for(int j = 0; j < n; j++){
+                    String temp = "";
+                    for(int k = 0; k < m; k++){
+                        if((i & 1 << k) > 0){ 
+                            temp += (relation[j][k] + " ");
+                        }
+                    }
+                    set.add(temp);
+                }
+                if(set.size() == n && check(i)){
+                    answer.add(i);
+                }
+            }
+            return answer.size();
+        }
+        
+        boolean check(int i) {
+            for (int j : answer) {
+                if ((i & j) == j) return false;
+            }
+            return true;
+        }
+    }
