b488db4622383307c0760513dc80e468222bf1db
조아빈 <whdkqls122@naver.com>
2025-11-03 21:00:41 +0900

    [BOJ]_10810_공 넣기

---
+++ November/Week01/Ahbeen/B3_10810.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+		for(int a = 0; a < M; a++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			for(int b = i; b <= j; b++) {
+				arr[b - 1] = k;
+			}
+		}
+		for(int a : arr) {
+			System.out.print(a + " ");
+		}
+	}
+}
