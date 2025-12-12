5aee8c32fef62117e9464370c30d2b7e4d91602d
SoHyun Park <pshsh0516@naver.com>
2025-12-12 16:32:38 +0900

    [BOJ]_2302_극장 좌석

---
+++ December/Week2/SoHyun/G5_2302.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+vip = [int(input()) for _ in range(m)]
+
+dp = [0] * (n+1)
+dp[0] = 1
+dp[1] = 1
+if n >= 2:
+    dp[2] = 2
+
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+    
+answer = 1
+if m > 0:
+    pre = 0
+    for j in range(m):
+        answer *= dp[vip[j] - 1 - pre]
+        pre = vip[j]
+    answer *= dp[n - pre]
+else:
+    answer = dp[n]
+print(answer)
