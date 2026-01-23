dba8961cd08afa3e59e5f0774faceb06452d3771
조아빈 <whdkqls122@naver.com>
2026-01-23 16:46:13 +0900

    [BOJ]_13913_숨바꼭질4

---
+++ January/Week3/AhBeen/G4_13913.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	static final int MAX = 100000;
+	static int[] parent;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		parent = new int[MAX + 1];
+		System.out.println(bfs(N, K));
+		Stack<Integer> stack = new Stack<>();
+		int cur = K;
+
+		while (cur != N) {
+			stack.push(cur);
+			cur = parent[cur];
+		}
+		stack.push(N);
+
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			sb.append(stack.pop()).append(" ");
+		}
+		System.out.println(sb);
+	}
+
+	static int bfs(int N, int K) {
+		boolean[] visited = new boolean[MAX + 1];
+		int[] dist = new int[MAX + 1];
+
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(N);
+		visited[N] = true;
+
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+
+			if(cur == K) {
+				return dist[cur];
+			}
+			int[] next = {cur - 1, cur + 1, cur * 2};
+			for(int nx : next) {
+				if(nx < 0 || nx > MAX) continue;
+				if(visited[nx]) continue;
+
+				visited[nx] = true;
+				dist[nx] = dist[cur] + 1;
+				parent[nx] = cur;
+				q.offer(nx);
+			}
+		}
+		return -1;
+	}
+}
