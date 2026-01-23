edd4c3d6282181e466e633ad6de67cb4ec9c62e0
SoHyun Park <pshsh0516@naver.com>
2026-01-24 01:07:58 +0900

    [BOJ]_14719_빗물

---
+++ January/Week3/SoHyun/G5_14719.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+h, w = map(int, input().split())
+block = list(map(int, input().split()))
+answer = 0
+
+for i in range(1, w-1):
+    left = max(block[:i])
+    right = max(block[i+1:])
+    m = min(left, right)
+    
+    if m > block[i]:
+        answer += m - block[i]
+        
+print(answer)
