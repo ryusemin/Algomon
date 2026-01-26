d55c3390ac0d23b26c53c6aca311641ad218682b
SoHyun Park <pshsh0516@naver.com>
2026-01-26 23:38:13 +0900

    [BOJ]_1058_친구

---
+++ January/Week4/SoHyun/S2_1058.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(input()) for _ in range(n)]
+f = [[0] * n for _ in range(n)]
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if graph[i][j] == 'Y' or (graph[i][k] == 'Y' and graph[k][j] =='Y'):
+                f[i][j] = 1
+                
+res = 0
+
+for row in f:
+    res = max(res, sum(row))
+print(res)
