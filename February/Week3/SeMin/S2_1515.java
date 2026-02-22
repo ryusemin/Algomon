f8f701f0903ee8041779008de213bb743d10a365
ryusemin <tpals3799@naver.com>
2026-02-22 22:58:09 +0900

    [BOJ]_1515_수 이어 쓰기

---
+++ February/Week3/SeMin/S2_1515.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int pt = 0;
+
+        int base = 0;
+        while (base++ <= 30000) {
+            String tmp = String.valueOf(base);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == s.charAt(pt))
+                    pt++;
+                if (pt == s.length()) {
+                    System.out.println(base);
+                    return;
+                }
+            }
+        }
+
+    }
+}
