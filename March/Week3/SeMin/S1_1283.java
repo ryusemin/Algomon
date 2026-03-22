a0bf085622f02eb16307ebd98a7aba99a4b6d9ad
ryusemin <tpals3799@naver.com>
2026-03-22 21:04:30 +0900

    [BOJ]_1188_음식 평론가

--- March/Week3/SeMin/S1_1283.java
+++ March/Week3/SeMin/S1_1283.java
@@ -1,62 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    static Set<Character> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        // 옵션의 개수
+        int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder ans = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		boolean[] alpha = new boolean[26];
-		xx: for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			String[] input = str.split(" "); 
+        for (int i = 0; i < N; i++) {
+            boolean first = false;
+            String option = br.readLine();
+			sb.append(option);
 
-			for (int j = 0; j < input.length; j++) {
-				int a = input[j].charAt(0)-'A'; 
-				if(a >= 26) a -= 32;
-				if (!alpha[a]) {
-					alpha[a] = true;
-					StringBuilder sb = new StringBuilder();
-					sb.append(input[j]);
-					sb.insert(0, '[');
-					sb.insert(2, ']');
-					input[j] = sb.toString();
-					for (int k = 0; k < input.length; k++) {
-						ans.append(input[k]).append(' ');
-					}
-					ans.append('\n');
-					continue xx;
-				}
-			}
+            st = new StringTokenizer(option);
 
-			for (int j = 0; j < input.length; j++) {
-				for (int k = 1; k < input[j].length(); k++) {
-					int a = input[j].charAt(k)-'A';
-					if(a > 26) a -= 32;
-					if (!alpha[a]) { 
-						alpha[a] = true;
-						StringBuilder sb = new StringBuilder();
-						sb.append(input[j]);
-						sb.insert(k, '[');
-						sb.insert(k + 2, ']');
-						input[j] = sb.toString();
-						for (int l = 0; l < input.length; l++) {
-							ans.append(input[l]).append(' ');
-						}
-						ans.append('\n');
-						continue xx;
-					}
-				}
-			}
+            // 1. 첫 글자가 단축키로 지정되었는지 
+            while (st.hasMoreTokens()) {
+                String s = st.nextToken();
+                char check = s.charAt(0);
+                if(!set.contains(check)){
+                    addSet(check);
+                    
+                    int idx = option.indexOf(s); // option에서 해당 단어의 시작 인덱스
+                    sb.insert(idx + 1, "]");
+                    sb.insert(idx, "[");
+                    first = true;
+                    break;
+                }
+            }
+            if(first) {
+                System.out.println(sb);
+                sb.setLength(0);
+                continue;
+            }
+            st = new StringTokenizer(option);
+            
+            // 2. 왼쪽부터 차례대로 알파벳 보며
+            for (int j = 0; j < option.length(); j++) {
+                char check = option.charAt(j);
+                if (check == ' ') continue; // 공백 스킵
+                if (j == 0) continue;       // 첫 글자는 1번에서 이미 처리
+                if (!set.contains(check)) {
+                    addSet(check);
 
-			for (int l = 0; l < input.length; l++) {
-				ans.append(input[l]).append(' ');
-			}
-			ans.append('\n');
-		}
-		System.out.println(ans.toString());
-	}
+					sb.insert(j + 1, "]");
+                	sb.insert(j, "[");
+					break;
+                }
+            }
 
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+        
+    }
+
+    // 단축키 지정 (Set)
+    static void addSet(char c){
+        set.add(Character.toLowerCase(c));
+        set.add(Character.toUpperCase(c));
+    }
 }
