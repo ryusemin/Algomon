60a1b2d13a959c8a0e25c31c169a94cf77b64f59
조아빈 <whdkqls122@naver.com>
2026-03-23 23:22:33 +0900

    [BOJ]_1149_RGB거리

---
+++ March/Week4/AhBeen/S1_1149.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] cost = new int[N][3];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i = 1; i < N; i++) {
+			cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+			cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+			cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
+		}
+
+		System.out.println(Math.min(Math.min(cost[N - 1][0], cost[N - 1][1]), cost[N - 1][2]));
+	}
+}
