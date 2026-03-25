164b2988a63e768aeb0ba0737e26bd508370d26d
SoHyun Park <pshsh0516@naver.com>
2026-03-25 15:28:58 +0900

    [BOJ]_1149_RGB거리

---
+++ March/Week4/SoHyun/S1_1149.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0] * n
+
+for i in range(n):
+    dp[i] = list(map(int, input().split()))
+    
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
+    
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
