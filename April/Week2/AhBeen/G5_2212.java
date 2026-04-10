dce421f2989f078d2d61a0129c1a4b951df1f482
조아빈 <whdkqls122@naver.com>
2026-04-10 20:41:31 +0900

    [BOJ]_2212_센서

--- April/Week2/AhBeen/G5_2212.java
+++ April/Week2/AhBeen/G5_2212.java
@@ -5,37 +5,21 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-		int K = Integer.parseInt(br.readLine());
-
-		if(K >= N) {
-			System.out.println(0);
-			System.exit(0);
-		}
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[] sensors = new int[N];
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+
+		Long[][] arr = new long[N][2];
+
 		for(int i = 0; i < N; i++) {
-			sensors[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(sensors);
-
-		Integer[] dist = new Integer[sensors.length - 1];
-
-		for(int i = 0 ; i < N - 1; i++) {
-			dist[i] = sensors[i + 1] - sensors[i];
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Long.parseLong(st.nextToken());
+			arr[i][1] = Long.parseLong(st.nextToken());
 		}
 
-		Arrays.sort(dist, Comparator.reverseOrder());
+		Arrays.sort(arr, (a1, a2) -> a1[0].compareTo(a2[0]));
 
-		int answer = 0;
-
-		for(int i =  K - 1; i < dist.length; i++) {
-			answer += dist[i];
-		}
-
-		System.out.println(answer);
+		System.out.println(arr);
 	}
 }
