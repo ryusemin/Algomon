fbd6e78385a015d3848f6855a55ef186d14a2d56
조아빈 <whdkqls122@naver.com>
2026-04-20 17:40:06 +0900

    [BOJ]_2579_계단 오르기

---
+++ April/Week4/AhBeen/S3_2579.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		if(N == 1) {
+			System.out.println(arr[0]);
+			return;
+		}
+
+		dp[0] = arr[0];
+		dp[1] = arr[0] + arr[1];
+
+		if(N >= 3) {
+			dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
+		}
+
+		for(int i = 3; i < N; i++) {
+			dp[i] = Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+		}
+		System.out.println(dp[N - 1]);
+	}
+}
