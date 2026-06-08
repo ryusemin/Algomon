4aa06c14bf682b8cc5cfbb88c6757af9207dd3e1
조아빈 <whdkqls122@naver.com>
2026-06-08 23:33:28 +0900

    fix: 잘못된 코드 수정

--- June/Week2/AhBeen/L11_돌아가는 팔각 의자.java
+++ June/Week2/AhBeen/L11_돌아가는 팔각 의자.java
@@ -1,51 +1,116 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long B = sc.nextLong();
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
+	static class Chair {
+		int idx;
+		int dir;
+
+		Chair(int idx, int dir) {
+			this.idx = idx;
+			this.dir = dir;
 		}
+	}
 
-		Arrays.sort(a);
+	static List<Chair> rotateInfo;
+	static int[][] chairs = new int[5][8];
 
-		long low = a[0];
-		long high = a[n - 1] + (long) Math.sqrt(B);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		long ans = 0;
 
-		while(low <= high) {
-			long mid = (low + high) / 2;
-
-			if(calc(mid, a, B)) {
-				low = mid + 1;
-				ans = mid;
-			} else {
-				high = mid - 1;
+		for(int i = 1; i <= 4; i++) {
+			String line = br.readLine();
+			for(int j = 0; j < 8; j++) {
+				chairs[i][j] = line.charAt(j) - '0';
 			}
 		}
 
-		System.out.println(ans);
+		int k = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < k; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int n = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+
+			rotateInfo = new ArrayList<>();
+			rotateInfo.add(new Chair(n, d));
+
+			searchL(n, d);
+			searchR(n, d);
+
+			for(Chair c : rotateInfo) {
+				if(c.dir == 1) {
+					rotateR(c);
+				} else {
+					rotateL(c);
+				}
+			}
+		}
+
+		int sum = 0;
+
+		for(int i = 1; i < 5; i++) {
+			sum += (int) Math.pow(2, i - 1) * chairs[i][0];
+		}
+
+		System.out.println(sum);
 	}
 
-	static boolean calc(long mid, int[] a, long B) {
-		long total = 0;
+	static void searchL(int n, int d) {
+		int curD = d;
+		for(int i = n; i > 1; i--) {
+			curD = curD * -1;
+			if(chairs[i][6] != chairs[i - 1][2]) {
+				rotateInfo.add(new Chair(i - 1, curD));
+			} else {
+				break;
+			}
+		}
+	}
 
-		for(int num : a) {
-			if (num >= mid) break;
+	static void searchR(int n, int d) {
+		int curD = d;
+		for(int i = n; i < 4; i++) {
+			curD = curD * -1;
+			if(chairs[i][2] != chairs[i + 1][6]) {
+				rotateInfo.add(new Chair(i + 1, curD));
+			} else {
+				break;
+			}
+		}
+	}
 
-			total += Math.pow(mid - num, 2);
+	static void rotateR(Chair c) {
+		int idx = c.idx;
 
-			if(total > B) return false;
+		int[] chair = chairs[idx];
+
+		int tmp = chair[7];
+
+		for(int i = 7; i >= 1; i--) {
+			chair[i] = chair[i - 1];
 		}
 
-		if(total > B) {
-			return false;
-		} else {
-			return true;
+		chair[0] = tmp;
+
+		chairs[idx] = chair;
+	}
+
+	static void rotateL(Chair c) {
+		int idx = c.idx;
+
+		int[] chair = chairs[idx];
+
+		int tmp = chair[0];
+
+		for(int i = 0; i < 7; i++) {
+			chair[i] = chair[i + 1];
 		}
+
+		chair[7] = tmp;
+
+		chairs[idx] = chair;
 	}
 }
